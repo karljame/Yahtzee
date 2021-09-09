910e95d05bec2a30320ec2d073ab0914173b230d
karljame <karl321jame@gmail.com>
2021-09-09 11:35:03 +0300

    dice work !!!!!!

--- index.js
+++ index.js
@@ -62,3 +62,45 @@ rollBtn.addEventListener('click', () => {
     }
     turnsDiv.innerText = rollCount;
 })
+
+
+let diceData = [
+  {
+    value: undefined,
+    status: "active"
+  },
+  {
+    value: undefined,
+    status: "active"
+  },
+  {
+    value: undefined,
+    status: "active"
+  },
+  {
+    value: undefined,
+    status: "active"
+  },
+  {
+    value: undefined,
+    status: "active"
+  },
+]
+
+const diceDivs = document.querySelectorAll('.dice')
+
+diceDivs.forEach( (dice) => {
+  dice.addEventListener('click' , () => {
+
+    if (dice.dataset.status == 'active') {
+      dice.dataset.status = 'locked';
+      diceData[dice.dataset.index].status = 'locked';
+
+    } else {
+      dice.dataset.status = 'actuve';
+      diceData[dice.dataset.index].status = 'active';
+    }
+
+    console.log(diceData)
+  })
+})
